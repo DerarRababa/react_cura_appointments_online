46a7afafd8c822722f0c65826ef31ca2ee78a26c
Derar Rababah <derar@abwaab.me>
2022-07-27 16:07:56 +0300

    Fix loading and fetch data

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,7 +1,13 @@
+import axios from 'axios'
 
 
-export const addTodo = text => ({
-    type: 'ADD_TODO'
-  })
+export const fetchData = (dispatch)  => {
+    axios.get(`${process.env.REACT_APP_API_END_POINT}`).then((res)=>{
+        dispatch({
+          type: 'ADD_DATA',
+          data: JSON.parse(res.data)
+        })
+     }) 
+}
   
 
